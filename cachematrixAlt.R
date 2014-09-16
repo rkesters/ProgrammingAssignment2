39e459701486f3ff4b4295583ddf47d57749fd61
Robert Kesterson <rkesters@gmail.com>
2014-09-15 22:30:26 -0400

    enhanced assignment 2

---
+++ cachematrixAlt.R
@@ -0,0 +1,81 @@
+## The following two functions enable the caching of the inversion of a matrix
+## once the inversion has been calcuated
+
+## This function creates an object that will store the matrix and the cached inverse
+
+makeCacheMatrix <- function(x = matrix()) {
+    inv <- NULL
+    set <- function(y) {
+
+## Only need to change x if y is different
+        if( !identical(x,y)){
+            x <<- y
+            inv <<- NULL
+        }
+    }
+    get <- function() x
+    setInverse <- function(inverse) inv <<- inverse
+    getInverse <- function() inv
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
+}
+
+
+## This function will frist check if an inverse has been cached. If it has than 
+## it will be returned, if not then the inverse will be calculated, cached
+## and returned.
+
+cacheSolve <- function(x, ...) {
+    inv <- x$getInverse()
+    if(!is.null(inv)) {
+        message("getting cached data")
+            return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setInverse(inv)
+    inv
+}
+
+## While the above functions are what the assingment requested, the structure is
+## odd from an Object Oriented Prgromming point of view. The following is another
+## implementation, with just one function. I have added three functions to the 
+## cacheMatrix object to allow for setting and solving, and to setSolve in one 
+## call.
+
+makeCacheMatrixV2 <- function(x = matrix()) {
+    inv <- NULL
+    set <- function(y) {
+
+## Only need to change x if y is different
+        if ( !identical(x , y)) {
+            x <<- y
+            inv <<- NULL
+        }       
+    }
+    setSolve <- function(y,...) {
+
+## Only need to change x if y is different
+        if ( !identical (x , y)) {
+            x <<- y
+            inv <<- NULL
+        }       
+        Solve(...)
+    }
+    Solve <- function(...) {
+        if(!is.null(inv)) {
+            message("getting cached data")
+                return(inv)
+        }
+        inv <<- solve(x, ...)
+        inv
+    }
+    get <- function() x
+    setInverse <- function(inverse) inv <<- inverse
+    getInverse <- function() inv
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse, Solve = Solve, 
+         setSolve = setSolve)
+}
